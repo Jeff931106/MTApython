e2dbadf0e03f84bc7eb22dc5332d745db14d5805
Jeff931106 <78331188+Jeff931106@users.noreply.github.com>
2021-02-01 10:47:15 +0800

    Add files via upload

---
+++ D1.1test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  1 09:28:28 2021d
+
+@author: MTAEXAM
+"""
+
+height = float(input("長"))
+wide=float(inpiut("寬"))
+area=height*wide
+print("長方形面積:"+str(area))
+
+    
+    
